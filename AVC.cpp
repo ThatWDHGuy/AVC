eb9458a7de1a5a5409dcc621fe583a8e13da554e
William <Will@oceanplait.com>
2019-05-30 14:30:17 +1200

    Will's rework

--- AVC.cpp
+++ AVC.cpp
@@ -7,6 +7,7 @@ using namespace std;
 int quadrant = 1;
 const int leftMotor = 5;
 const int rightMotor = 1;
+int speed = 0;
 
 void openGate(){
 	char gateIP[] = "130.195.6.196";
@@ -28,21 +29,24 @@ void openGate(){
 */
 
 //Set motors to make robot go straight ahead
-void forward() {
-	set_motors(leftMotor, 33);
-	set_motors(rightMotor, 63); 
+void setForward() {
+	cout<<"Setting motor forward"<<endl;
+	set_motors(leftMotor, 40);
+	set_motors(rightMotor, 56); 
 	hardware_exchange();
 }
 
 //Set motors to make robot go back
-void reverse() {
-	set_motors(leftMotor, 63);
-	set_motors(rightMotor, 33);
+void setReverse() {
+	cout<<"Setting motor reverse"<<endl;
+	set_motors(leftMotor, 56);
+	set_motors(rightMotor, 40);
 	hardware_exchange();
 }
 
 // Make a 90 degree hard left turn
-void hardLeft() {
+void doHardLeft() {
+	cout<<"Turning Hard Left"<<endl;
 	set_motors(leftMotor, 35); 
 	set_motors(rightMotor, 35); 
 	hardware_exchange();
@@ -53,7 +57,8 @@ void hardLeft() {
 }
 
 // Make a 90 degree hard right turn
-void hardRight() {
+void doHardRight() {
+	cout<<"Turning Hard Right"<<endl;
 	set_motors(leftMotor, 61);
 	set_motors(rightMotor, 61);
 	hardware_exchange();
@@ -63,15 +68,17 @@ void hardRight() {
 	hardware_exchange();
 }
 
-void left(int speed) {
-	set_motors(leftMotor, 48-((int)speed/2));
-	set_motors(rightMotor, 48+speed);
+void turnLeft() {
+	cout<<"Turning Left"<<endl;
+	set_motors(leftMotor, 41));
+	set_motors(rightMotor, 55);
 	hardware_exchange();
 }
 
-void right(int speed) {
-	set_motors(leftMotor, 48-speed);
-	set_motors(rightMotor, 48+((int)speed/2));
+void turnRight() {
+	cout<<"Turning Right"<<endl;
+	set_motors(leftMotor, 55);
+	set_motors(rightMotor, 41));
 	hardware_exchange();
 }
 
@@ -80,81 +87,49 @@ const int camHeight = 240;
 const int turns[] = {0, 0, 1, 1, 0, 1, 0}; // 0 = right, 1 = left
 int turnsTaken = 0;
 
-//Alternate follow line method, using single line idea
-void followLine() {
-	int total = 0;
-	int speed = 0;
-	int blackCountMid = 0;
-	int blackCountTop = 0;
-	int redCount = 0;
-	
+void followLine() { //Q2
+	int linePos = 0;
 	take_picture();
 	//update_screen(); TESTING
 	
 	for (int x=0; x<camWidth; x++) {
 		if ((int)get_pixel(((int)camHeight/2), x, 3) < 100) {
-			total += x-camWidth/2;
-			blackCountMid ++;
-		}
-		if ((int)get_pixel(((int)camHeight/5), x, 3) < 100 && quadrant == 2) {
-			blackCountTop ++;
-		}
-		if (((int)get_pixel(((int)camHeight/2), x, 0))-80 > ((int)get_pixel(((int)camHeight/2), x, 1)) && ((int)get_pixel(((int)camHeight/2), x, 0))-80 > ((int)get_pixel(((int)camHeight/2), x, 2))) {
-			redCount++;
+			if (x < camWidth/2){
+				linePos--;
+			} else {
+				linePos++;
+			}
 		}
 	}
-	
-	if (blackCountTop < 10 && blackCountMid > 40 && quadrant == 2) {
-		quadrant = 3;
-	}
-	if (redCount > 5) {
-		quadrant = 4;
-	}
-	if (quadrant == 3) {
-		if (blackCountMid > ((int)(camWidth/2.5))) {
-			if (turns[turnsTaken] == 1) {
-				hardLeft();
-			}
-			if (turns[turnsTaken] == 0) {
-				hardRight();
-			}
-			turnsTaken++;
-		} else {
-			forward();
-		}
-	} else {
-		speed = (int)(total*18/camWidth);	
-		if (abs(speed) > 4) {
-			if (speed < 0) {
-				left(abs(speed));
-			}
-			if (speed > 0) {
-				right(speed);
-			}
-		} else { 
-			forward();
-		}
-	}
+	cout<<linePos<<endl;
+	if (linePos == 0){
+		setForward();
+	} else if (linePos > 0) {
+		turnRight();
+	} else if (linePos < 0) {
+		turnLeft();
+	}	
+}
+void detectNextQ(){
+	//look for red
 }
 
 //Get everything going
 int main() {
 	init(0);
 	//open_screen_stream(); TESTING
+	quadrant = 2;
 	while (true) {
-		
-			/*
 			if (quadrant == 1) {
 					openGate();
 					quadrant = 2;
-			} else if (quadrant == 2 || quadrant == 3) {
-					followLine();
+			} else if (quadrant == 2)
+				followLine();
+			} else if (quadrant == 3) {
+					
 			} else if (quadrant == 4) {
 
 			}
-			*/
-			
-			followLine();
 			
 			/* TESTING
 			set_motors(1,56);
